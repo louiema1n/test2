2de21ca5464c76c2f4270f9270e750552260ffe4
louiema1n <louielcxy@live.cn>
2016-12-07 14:08:36 +0800

    删除

--- day36_spring_ss/src/com/lm/dao/AccountDao.java
+++
@@ -1,7 +0,0 @@
-package com.lm.dao;
-
-import com.lm.domain.Account;
-
-public interface AccountDao {
-	public void save(Account account);
-}
--- day36_spring_ss/src/com/lm/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.lm.service;
-
-import com.lm.domain.Account;
-
-
-public interface AccountService {
-	public void add(Account account);
-}
